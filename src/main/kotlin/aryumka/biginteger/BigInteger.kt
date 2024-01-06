2f8103de0dc9e412a5a649ed72e9d27f743c369b
aryumka <yooooar@gmail.com>
2024-01-06 18:02:17 +0900

    Implement factory methods

--- src/main/kotlin/aryumka/biginteger/BigInteger.kt
+++ src/main/kotlin/aryumka/biginteger/BigInteger.kt
@@ -1,6 +1,20 @@
 package aryumka.biginteger
 
 class BigInteger(
-
+  private val value: String
 ) {
+  // Factory methods
+  companion object {
+    fun of(value: String): BigInteger {
+      return BigInteger(value)
+    }
+
+    fun of(value: Int): BigInteger {
+      return BigInteger(value.toString())
+    }
+
+    fun of(value: Long): BigInteger {
+      return BigInteger(value.toString())
+    }
+  }
 }
