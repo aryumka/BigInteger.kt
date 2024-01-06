4397b4a53ea577f0589704b0c542d29349562c36
aryumka <yooooar@gmail.com>
2024-01-06 18:07:31 +0900

    Implements BigIntegerTests

--- src/test/kotlin/aryumka/biginteger/BigIntegerTests.kt
+++ src/test/kotlin/aryumka/biginteger/BigIntegerTests.kt
@@ -1,4 +1,28 @@
 package aryumka.biginteger
 
-class BigIntegerTests {
-}
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class BigIntegerTests: FunSpec({
+  context("of Factory") {
+    test("Int to BigInteger") {
+      val bigInteger = BigInteger.of(1)
+
+      bigInteger.toString() shouldBe "1"
+    }
+
+    test("BigInteger.of(1) == BigInteger.of(\"1\")") {
+      val bigInteger1 = BigInteger.of(1)
+      val bigInteger2 = BigInteger.of("1")
+
+      bigInteger1 shouldBe bigInteger2
+    }
+
+    test("BigInteger.of(1) == BigInteger.of(1L)") {
+      val bigInteger1 = BigInteger.of(1)
+      val bigInteger2 = BigInteger.of(1L)
+
+      bigInteger1 shouldBe bigInteger2
+    }
+  }
+})
