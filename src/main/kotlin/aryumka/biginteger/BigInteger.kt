36abeedf3711403fdb30c69e7617a5b97d42eccd
aryumka <yooooar@gmail.com>
2024-01-06 18:51:43 +0900

    Implement plus methods

--- src/main/kotlin/aryumka/biginteger/BigInteger.kt
+++ src/main/kotlin/aryumka/biginteger/BigInteger.kt
@@ -18,9 +18,36 @@ class BigInteger(
     }
   }
 
-  operator fun plus(other: BigInteger): BigInteger {
-    TODO()
+  private fun plus(other: String): BigInteger {
+    var result = ""
+    var carry = 0
+    var valueLength = this.value.length - 1
+    var otherLength = other.length - 1
+
+    while (valueLength >= 0 || otherLength >= 0 || carry > 0) {
+      val valueDigit = if (valueLength >= 0) this.value[valueLength] - '0' else 0
+      val otherDigit = if (otherLength >= 0) other[otherLength] - '0' else 0
+
+      val sum = valueDigit + otherDigit + carry
+
+      result += (sum % 10)
+      carry = sum / 10
+
+      valueLength--
+      otherLength--
+    }
+
+    return BigInteger(result.reversed())
   }
+
+  operator fun plus(other: Int): BigInteger =
+    this.plus(other.toString())
+
+  operator fun plus(other: Long): BigInteger =
+    this.plus(other.toString())
+
+  operator fun plus(other: BigInteger): BigInteger =
+    this.plus(other.value)
 
   override fun equals(other: Any?): Boolean =
     when (other) {
